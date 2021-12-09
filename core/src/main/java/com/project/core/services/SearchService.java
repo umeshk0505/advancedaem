447898761ab1cf522c297c07a4980c47bbf097bc
umeshk0505 <90315693+umeshk0505@users.noreply.github.com>
2021-12-09 03:32:39 +0000

    Delete SearchService.java

--- core/src/main/java/com/project/core/services/SearchService.java
+++
@@ -1,7 +0,0 @@
-package com.project.core.services;
-
-import org.json.JSONObject;
-
-public interface SearchService {
-    public JSONObject searchResult(String searchText,int startResult,int resultPerPage);
-}
